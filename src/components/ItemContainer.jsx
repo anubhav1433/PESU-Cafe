e25bd1c1b8671dda3b738a6040f7f0889a9ff4eb
Sarthak S Kumar <sskworld9742@gmail.com>
2023-03-06 14:46:00 +0530

    add fetch API in ItemContainer

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,31 +1,27 @@
 import React from "react";
 import ProductCard from "./ProductCard.jsx";
-export default function ItemContainer(props) {
-  async function fetchData() {
-    // const title = props.title;
-    // try {
-    //   const res = await fetch("./Assets/data.json");
-    //   const data = await res.json();
-    //   console.log(Object.entries(data..items)[0][1]); //productID
-    // } catch (err) {
-    //   console.error(err);
-    // }
+export default class ItemContainer extends React.Component {
+  componentDidMount() {
+    fetch("./Assets/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ products: data });
+      })
+      .catch((error) => console.log(error));
+    console.log(this.state.products);
   }
-
-  return (
-    <div className="mx-16 p-6">
-      <p
-        align="left"
-        className="text-4xl m-4 text-gray-800 font-bold hover:text-gray-700"
-      >
-        {props.title}
-      </p>
-      <hr />
-      <div className="flex flex-wrap justify-around w-full my-4">
+  render() {
+    return (
+      <div className="mx-16 p-6">
+        <p
+          align="left"
+          className="text-4xl m-4 text-gray-800 font-bold hover:text-gray-700"
+        ></p>
+        {}
         <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <hr />
+        <div className="flex flex-wrap justify-around w-full my-4"></div>
       </div>
-    </div>
-  );
+    );
+  }
 }

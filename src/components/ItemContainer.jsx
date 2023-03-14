b39d6b4e6c64961d5893574e2d37b77f50d5ab41
Sarthak S Kumar <sskworld9742@gmail.com>
2023-03-14 14:58:21 +0530

    fix fetch API in itemContainer

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 import ProductCard from "./ProductCard.jsx";
-export default class ItemContainer extends React.Component {
-  componentDidMount() {
-    fetch("./Assets/data.json")
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({ products: data });
+import { useEffect, useState } from "react";
+
+const ItemContainer = (props) => {
+  const [products, setProducts] = useState("");
+
+  const fetchData = () => {
+    fetch("/Assets/data.json")
+      .then((response) => {
+        return response.json();
       })
-      .catch((error) => console.log(error));
-    console.log(this.state.products);
-  }
-  render() {
-    return (
-      <div className="mx-16 p-6">
-        <p
-          align="left"
-          className="text-4xl m-4 text-gray-800 font-bold hover:text-gray-700"
-        ></p>
-        {}
-        <ProductCard />
-        <hr />
-        <div className="flex flex-wrap justify-around w-full my-4"></div>
-      </div>
-    );
-  }
-}
+
+      .then((data) => {
+        setProducts(data);
+        console.log(products.Munchies);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="mx-16 p-6">
+      <p
+        align="left"
+        className="text-4xl m-4 text-gray-800 font-bold hover:text-gray-700"
+      >
+        {props.title}
+      </p>
+      <ProductCard />
+      <hr />
+      <div className="flex flex-wrap justify-around w-full my-4"></div>
+    </div>
+  );
+};
+
+export default ItemContainer;

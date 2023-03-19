51f1cf37402405fb98a075016dd39fd8f97a0ae7
Sarthak S Kumar <sskworld9742@gmail.com>
2023-03-19 19:52:40 +0530

    change icons from unicons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,13 +20,7 @@ export default function Navbar(props) {
             onClick={() => setShowNav((prevState) => !prevState)}
             className="flex items-center px-3 py-2 border-2 rounded text-blue-900 border-blue-900 hover:text-blue-900 hover:border-blue-900"
           >
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
+            <i class="uil uil-bars"></i>
           </button>
         </div>
       </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -61,20 +61,7 @@ const ProductCard = (props) => {
             }}
             className="bg-white rounded-l border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M20 12H4"
-              />
-            </svg>
+            <i class="uil uil-minus"></i>
           </button>
           <div className="bg-gray-100 border-t border-b border-gray-100 text-gray-600 hover:bg-gray-100 inline-flex items-center px-4 py-1 select-none">
             {count}
@@ -88,20 +75,7 @@ const ProductCard = (props) => {
             }}
             className="bg-white rounded-r border text-gray-600 hover:bg-gray-100 active:bg-gray-200 disabled:opacity-50 inline-flex items-center px-2 py-1 border-r border-gray-200"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 4v16m8-8H4"
-              />
-            </svg>
+            <i class="uil uil-plus"></i>
           </button>
         </div>
         <button
@@ -110,20 +84,7 @@ const ProductCard = (props) => {
           className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 active:bg-blue-700 disabled:opacity-50 mt-4 w-full flex items-center justify-center"
         >
           Add to Cart
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 ml-2"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-            />
-          </svg>
+          <i class="uil uil-shopping-cart-alt p-1"></i>
         </button>
       </div>
     </div>

4f1e9d979d318606a4604f0b679d5056d04765b4
Anubhav Mendhiratta <anubhavmendhiratta@gmail.com>
2023-04-12 16:50:36 +0530

    CSS changes

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -6,6 +6,7 @@ import axios from "axios"
 const Login = ({ setLoginUser }) => {
 
     // const history = useHistory()
+    const [showRegister, setShowRegister] = useState(true)
 
     const [user, setUser] = useState({
         email: "",
@@ -20,26 +21,26 @@ const Login = ({ setLoginUser }) => {
         })
     }
 
-    // const login = () => {
-    //     axios.post("http://localhost:9002/login", user)
-    //         .then(res => {
-    //             alert(res.data.message)
-    //             setLoginUser(res.data.user)
-    //             history.push("/")
-    //         })
-    // }
+    const login = () => {
+        axios.post("http://localhost:9002/login", user)
+            .then(res => {
+                alert(res.data.message)
+                setLoginUser(res.data.user)
+            })
+    }
 
     return (
-        <div className="">
-
-            <div className="login">
-                <h2>Login</h2>
-                <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
-                <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter your Password" ></input>
-
-            </div>
-        </div>
-
+        
+        <div className="login hero">
+            <h2 className="text-blue-900 bg-white/50 indent-10 font-bold p-auto tracking-wider">Login Your Details</h2>
+            <input type="text" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
+            <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Enter your Password" ></input>
+            <a href="/"><button className="button bg-orange-500 hover:text-blue-900" onClick={login}>Login</button></a>
+            <div>Not a Member? <a className="underline text-blue-500" href="">SignUp</a></div>
+            
+        
+        
+</div>
 
     )
 }
--- src/components/userHandler/UserHandler.jsx
+++ src/components/userHandler/UserHandler.jsx
@@ -5,14 +5,13 @@ import Register from "../register/register"
 
 export default function UserHandler({}){
 const [showLogin, setShowLogin] = useState(true)
-const [showRegister, setShowRegister] = useState(false)
+
     return(
-        <div className="absolute top-[10rem] left-[10rem] rounded-lg w-3/4 text-center text-xl bg-white z-20">
-            <h1><span id="one">PESU</span><span id="two"> Cafe</span></h1>
-        <h3>🍴📱 Order your snacks and beverages <br/>from the PESU Cafeteria with
-            ease! 🍔🍟</h3>
-            <Login/>
-            <Register/> 
-        </div>
+            <div className="absolute bg-white/50 top-[5rem] left-[0rem] rounded-lg text-center text-xl z-20 p-[10px] justify-center w-[100%] h-[800px] overflow-hidden ">
+            <h1 class="bare_segment"><span id="one">PESU</span><span id="two"> Cafe</span></h1>
+            <h3>🍴📱 Order your snacks and beverages <br/>from the PESU Cafeteria with
+                ease! 🍔🍟</h3>
+                <Login/>
+            </div>
     )
 }

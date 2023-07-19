fcba64600bf2c161b89c9a37dd7052bebb7158b9
Anubhav Mendhiratta <anubhavmendhiratta@gmail.com>
2023-07-19 17:17:43 +0530

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar({ items, showLogin, showUserHandler }) {
     <nav className="flex items-center justify-between flex-wrap fixed top-0 z-10 w-full bg-white lg:py-4 pt-4 lg:px-12 shadow border-solid border-t-2 border-blue-900">
       <div className="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
         <div className="flex  items-center flex-shrink-0 text-gray-800 mr-16">
-          <a href="#"> <img className="animate-spin w-10 inline-block m-2" src="./img/Web/logo.png"></img></a>
+          <a href="#"> <img className="w-10 inline-block m-2" src="./img/Web/logo.png"></img></a>
           <span className="font-semibold text-2xl tracking-tight">
             <a href="#">
               PESU Cafe</a>
@@ -31,7 +31,7 @@ export default function Navbar({ items, showLogin, showUserHandler }) {
       >
 
         <div className="text-md font-bold text-blue-900 lg:flex-grow">
-          <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-900 mr-2"><button onClick={() => { showUserHandler((prev) => !prev) }}>Login</button></div>
+          {/* <div className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-900 mr-2"><button onClick={() => { showUserHandler((prev) => !prev) }}>Login</button></div> */}
           <a
             href="#"
             className="block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-900 mr-2"
